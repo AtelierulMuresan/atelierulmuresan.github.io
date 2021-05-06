d40ebf7ccc6d21b48df51cf5e5445a6371c64156
AtelierulMuresan <45881285+AtelierulMuresan@users.noreply.github.com>
2021-05-06 09:42:59 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,10 +116,19 @@ $(function() {
 	  copy.remove();
 	}, 500)
   })
-if (localStorage.getItem(‘cookieSeen’) != ‘shown’) {
-  $(‘.cookie-banner’).delay(2000).fadeIn();
-  localStorage.setItem(‘cookieSeen’,’shown’)
-};
-$(‘.close’).click(function() {
-  $(‘.cookie-banner’).fadeOut();
+// cookie policy
+$(document).on('ready', function() {
+  if (document.cookie.indexOf("accepted_cookies=") < 0) {
+    $('.cookie-overlay').removeClass('d-none').addClass('d-block');
+  }
+
+  $('.accept-cookies').on('click', function() {
+    document.cookie = "accepted_cookies=yes;"
+    $('.cookie-overlay').removeClass('d-block').addClass('d-none');
+  })
+
+  // expand depending on your needs
+  $('.close-cookies').on('click', function() {
+    $('.cookie-overlay').removeClass('d-block').addClass('d-none');
+  })
 })
