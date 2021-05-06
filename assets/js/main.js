51144b4ab93828280d6798232e18f0c8829243ec
AtelierulMuresan <45881285+AtelierulMuresan@users.noreply.github.com>
2021-05-06 09:40:14 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,10 @@ $(function() {
 	  copy.remove();
 	}, 500)
   })
+if (localStorage.getItem(‘cookieSeen’) != ‘shown’) {
+  $(‘.cookie-banner’).delay(2000).fadeIn();
+  localStorage.setItem(‘cookieSeen’,’shown’)
+};
+$(‘.close’).click(function() {
+  $(‘.cookie-banner’).fadeOut();
+})
